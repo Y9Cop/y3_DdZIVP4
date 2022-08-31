886720a3ca3267569cdeaa7375065aac9406eff5
iIoFOd <pgne375@gmail.com>
2022-08-31 20:20:05 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
 
-    const api_key = "1ZETs962931_9whKnMk4UZNSugXiGgNl"
+    const api_key = "nTI9IqajfJRKMXRucaCeNVv2hwL91Eig"
 
 
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
@@ -44,7 +44,7 @@ const Home = () => {
         method: 'GET'
       };
 
-      const api_key = "1ZETs962931_9whKnMk4UZNSugXiGgNl"
+      const api_key = "nTI9IqajfJRKMXRucaCeNVv2hwL91Eig"
       // const api_key = ""
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
